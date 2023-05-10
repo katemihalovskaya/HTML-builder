98af03f14a37922612364519ab254d15758685ae
Kate Mihalovskaya <kate.mihalovskaya@gmail.com>
2023-05-10 14:37:19 +0200

    fix: replace rimraf method

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,67 +4,14 @@ const path = require('path');
 const sourceFolder = './04-copy-directory/files';
 const destFolder = './04-copy-directory/files-copy';
 
-async function removeDirectory(dirPath) {
-  const dirExists = await directoryExists(destFolder);
-  console.log(dirExists);
-  if (dirExists) {
-    try {
-      const files = await fs.promises.readdir(dirPath);
-      console.log(files);
-      for (const file of files) {
-        const filePath = path.join(dirPath, file);
-        const fileStat = await fs.promises.lstat(filePath);
-        if (fileStat.isDirectory()) {
-          await removeDirectory(filePath);
-        } else {
-          await fs.promises.unlink(filePath);
-        }
-      }
-      await fs.promises.rmdir(dirPath);
-    } catch (err) {
-      console.error(`Error removing directory ${dirPath}: ${err}`);
-    }
-  }
-}
-
-removeDirectory(destFolder);
-
-/* async function copyFolder() {
-  const dirExists = await directoryExists(destFolder);
-  console.log(dirExists);
-  if(!dirExists) {
-    
-  } else {
-    copyFiles();
-  }
-} */
-
-function copyFolder() {
-  // Create the destination folder
-  fs.mkdir(destFolder, (err) => {
-    if (err) {
-      console.error(`Error creating directory: ${err}`);
-    } else {
-      copyFiles();
-    }
-  });
-}
-
-copyFolder();
-
-function directoryExists(dirPath) {
-  return new Promise((resolve) => {
-    fs.stat(dirPath, (err, stats) => {
-      if (err) {
-        resolve(false);
-      } else {
-        resolve(stats.isDirectory());
-      }
-    });
-  });
-}
-
-function copyFiles() {
+async function copyFiles() {
+  try {
+    await fs.promises.access(destFolder)
+      // Delete everything in the project-dist folder
+      await fs.promises.rmdir(destFolder, { recursive: true });
+  } catch (err) {}
+  
+  await fs.promises.mkdir(destFolder);
 // Copy files from the source folder to the destination folder
 fs.readdir(sourceFolder, (err, files) => {
   if (err) {
@@ -94,3 +41,5 @@ fs.readdir(sourceFolder, (err, files) => {
   }
 });
 }
+
+copyFiles();
